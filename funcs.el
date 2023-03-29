447f0c2af08ed700fc5d154b265286f94eb430ef
lawrsp <7957003+lawrsp@users.noreply.github.com>
2023-03-30 00:40:50 +0800

    fix

--- funcs.el
+++ funcs.el
@@ -2,8 +2,8 @@
 ;;
 ;; Copyright (c) 2012-2022 Sylvain Benner & Contributors
 ;;
-;; Author: Thanh Vuong <thanhvg@gmail.com>
-;; URL: https://github.com/thanhvg
+;; Author: lawrsp <lawrancersp@gmail.com>
+;; URL: https://github.com/lawrsp
 ;;
 ;; This file is not part of GNU Emacs.
 ;;
