6402ba577ddcef18e5f306fd1232822264b9de99
lawrsp <7957003+lawrsp@users.noreply.github.com>
2023-04-02 18:18:42 +0800

    change key-bindings because of conflicts

--- funcs.el
+++ funcs.el
@@ -22,33 +22,33 @@
 
 (defun spacemacs//mind-wave-setup-global-bindings ()
   "Define keys bindings globally of mind-wave document assistant functions"
-  (spacemacs/declare-prefix "mw"  "mind-wave")
+  (spacemacs/declare-prefix "mm"  "mind-wave")
   (spacemacs/set-leader-keys
     ;; Document mode
     ;; Selected Content:
     ;; Execute the command mind-wave-translate-to-english, ChatGPT will automatically replace the selected area with the translated content.
-    "mwt" 'mind-wave-translate-to-english
+    "mmt" 'mind-wave-translate-to-english
     ;; Execute the command mind-wave-proofreading-doc, ChatGPT will automatically replace the selected area with the polished document.
-    "mwp" 'mind-wave-proofreading-doc
+    "mmp" 'mind-wave-proofreading-doc
     ;; Execute the command mind-wave-explain-word, ChatGPT will automatically explain the meaning of the words in the current sentence and provide similar example sentences.
-    "mwe" 'mind-wave-explain-word
+    "mme" 'mind-wave-explain-word
     ;; Execute the command mind-wave-adjust-text. ChatGPT will adjust the text or code according to your instructions.
-    "mwa" 'mind-wave-adjust-text
+    "mma" 'mind-wave-adjust-text
     ;; restore window configuration
-    "mwR" 'mind-wave-restore-window-configuration))
+    "mmR" 'mind-wave-restore-window-configuration))
 
 (defun spacemacs//mind-wave-setup-prog-bindings ()
   "Define keys bindings of mind-wave prog assistant functions for mode"
   (let ((mode major-mode))
-    (spacemacs/declare-prefix-for-mode mode "mw" "mind-wave")
+    (spacemacs/declare-prefix-for-mode mode "mm" "mind-wave")
     (spacemacs/set-leader-keys-for-major-mode mode
       ;; Code Refactoring Mode
       ;; Move the cursor to the desired function for refactoring.
       ;; Execute the command mind-wave-refactory-code, ChatGPT will automatically split the screen to display the refactored code and suggestions for improvement on the right.
-      "wr" 'mind-wave-refactory-code
+      "mr" 'mind-wave-refactory-code
       ;; Execute the command mind-wave-comment-code, ChatGPT will automatically split the screen to display code with comments on the right.
-      "wc" 'mind-wave-comment-code
+      "mc" 'mind-wave-comment-code
       ;; Execute the command mind-wave-explain-code, ChatGPT will automatically split the screen to display an explanation for the code on the right.
-      "wE" 'mind-wave-explain-code
+      "mE" 'mind-wave-explain-code
       ;; Execute the command mind-wave-generate-commit-name, ChatGPT will automatically analyze the current diff content and generate a patch name.
-      "wg" 'mind-wave-generate-commit-name)))
+      "mg" 'mind-wave-generate-commit-name)))
